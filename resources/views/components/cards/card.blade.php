877f74cc2bf19de9a5fd6a8c7561986c2fb6f965
abdoul-rb <abdoulrahimbah6@gmail.com>
2025-05-10 21:17:32 +0200

    feat: update home with my template

---
+++ resources/views/components/cards/card.blade.php
@@ -0,0 +1,39 @@
+@props([
+    'title' => '',
+    'url' => '',
+    'description' => '',
+])
+
+<li
+    class="group relative flex flex-col items-start bg-neutral-900 border border-neutral-800 rounded-xl p-6 transition-all hover:ring-1 hover:ring-neutral-700">
+    <div class="flex items-center gap-2 leading-none font-semibold tracking-tight">
+        <img alt="Coolify favicon" loading="lazy" width="20" height="20" decoding="async" data-nimg="1"
+            class="rounded-sm" style="color:transparent"
+            src="https://www.google.com/s2/favicons?domain=www.coolify.io&amp;sz=128">
+
+        <h2 class="text-base font-semibold text-zinc-100">
+            {{-- <div
+            class="absolute -inset-x-4 -inset-y-6 z-0 scale-95 opacity-0 sm:-inset-x-6 sm:rounded-2xl bg-zinc-800/50 border-border/40 bg-muted/20 hover:bg-muted/30 group-hover:border-ring/20 ring-ring/8 flex h-full flex-col rounded-lg border p-6 shadow-sm transition-all hover:ring-[3px]">
+        </div> --}}
+            <a href="{{ $url }}">
+                <span class="absolute -inset-x-4 -inset-y-6 z-20 sm:-inset-x-6 sm:rounded-2xl"></span>
+                <span class="relative z-10">
+                    {{ $title }}
+                </span>
+            </a>
+        </h2>
+    </div>
+
+    <p class="relative z-10 mt-2 text-sm text-zinc-400">
+        {{ $description }}
+    </p>
+
+    <p class="relative z-10 mt-6 flex text-sm font-medium transition group-hover:text-teal-500 text-zinc-200">
+        <svg viewBox="0 0 24 24" aria-hidden="true" class="h-6 w-6 flex-none">
+            <path
+                d="M15.712 11.823a.75.75 0 1 0 1.06 1.06l-1.06-1.06Zm-4.95 1.768a.75.75 0 0 0 1.06-1.06l-1.06 1.06Zm-2.475-1.414a.75.75 0 1 0-1.06-1.06l1.06 1.06Zm4.95-1.768a.75.75 0 1 0-1.06 1.06l1.06-1.06Zm3.359.53-.884.884 1.06 1.06.885-.883-1.061-1.06Zm-4.95-2.12 1.414-1.415L12 6.344l-1.415 1.413 1.061 1.061Zm0 3.535a2.5 2.5 0 0 1 0-3.536l-1.06-1.06a4 4 0 0 0 0 5.656l1.06-1.06Zm4.95-4.95a2.5 2.5 0 0 1 0 3.535L17.656 12a4 4 0 0 0 0-5.657l-1.06 1.06Zm1.06-1.06a4 4 0 0 0-5.656 0l1.06 1.06a2.5 2.5 0 0 1 3.536 0l1.06-1.06Zm-7.07 7.07.176.177 1.06-1.06-.176-.177-1.06 1.06Zm-3.183-.353.884-.884-1.06-1.06-.884.883 1.06 1.06Zm4.95 2.121-1.414 1.414 1.06 1.06 1.415-1.413-1.06-1.061Zm0-3.536a2.5 2.5 0 0 1 0 3.536l1.06 1.06a4 4 0 0 0 0-5.656l-1.06 1.06Zm-4.95 4.95a2.5 2.5 0 0 1 0-3.535L6.344 12a4 4 0 0 0 0 5.656l1.06-1.06Zm-1.06 1.06a4 4 0 0 0 5.657 0l-1.061-1.06a2.5 2.5 0 0 1-3.535 0l-1.061 1.06Zm7.07-7.07-.176-.177-1.06 1.06.176.178 1.06-1.061Z"
+                fill="currentColor"></path>
+        </svg>
+        <span class="ml-2">{{ str_replace(['http://', 'https://'], '', $url) }}</span>
+    </p>
+</li>
---
+++ resources/views/partials/nav.blade.php
@@ -0,0 +1,39 @@
+<nav
+    class=" bg-[#100D08] bg-background/95 supports-[backdrop-filter]:bg-background/60 sticky top-0 z-50 border-b border-dashed border-white/10 backdrop-blur">
+    <div class="max-w-7xl mx-auto px-6 py-4 lg:px-0 flex items-center justify-between">
+        <div class="flex items-center gap-6">
+            <a href="#" class="flex items-center gap-2 font-bold text-lg text-white">
+                <span class="inline-block bg-[#23272e] rounded p-1">
+                    <svg width="28" height="28" viewBox="0 0 28 28" fill="none"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <rect width="28" height="28" rx="6" fill="#23272e" />
+                        <rect x="5" y="5" width="18" height="18" rx="4" fill="#fff" />
+                    </svg>
+                </span>
+                Clippr
+            </a>
+
+            <a href="#" class="text-gray-300 hover:text-white text-sm font-medium">
+                Tags
+            </a>
+        </div>
+
+        <div class="flex items-center gap-3">
+            <button class="bg-[#23272e] dark:bg-[#23272e] text-white text-xs px-3 py-1 rounded hover:bg-[#333] mr-2">
+                Sponsor spot
+            </button>
+
+            <a href="#" class="text-gray-400 hover:text-black dark:hover:text-white mr-2">
+                <svg width="20" height="20" fill="currentColor" viewBox="0 0 24 24">
+                    <path
+                        d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.387.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.416-4.042-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.084-.729.084-.729 1.205.084 1.84 1.236 1.84 1.236 1.07 1.834 2.809 1.304 3.495.997.108-.775.418-1.305.762-1.605-2.665-.305-5.466-1.334-5.466-5.931 0-1.31.469-2.381 1.236-3.221-.124-.303-.535-1.523.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.553 3.297-1.23 3.297-1.23.653 1.653.242 2.873.119 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.803 5.624-5.475 5.921.43.372.823 1.102.823 2.222 0 1.606-.014 2.898-.014 3.293 0 .322.216.694.825.576C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12" />
+                </svg>
+            </a>
+
+            <button
+                class="bg-black dark:bg-white text-white dark:text-black px-4 py-1 rounded text-sm font-semibold ml-2">
+                {{ __('Ajouter') }}
+            </button>
+        </div>
+    </div>
+</nav>
